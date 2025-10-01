143404f3afb9af614aa313ca152ab463e4d3d8f0
ssakamoto04510-commits <s.sakamoto04510@gmail.com>
2025-10-01 23:10:34 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ document.addEventListener("scroll", () => {
     }
   });
 });
+
+function openModal(imgElement) {
+  const modal = document.getElementById("modal");
+  const modalImg = document.getElementById("modal-img");
+  const caption = document.getElementById("modal-caption");
+
+  modal.style.display = "block";
+  modalImg.src = imgElement.src;
+  caption.textContent = imgElement.alt;
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
